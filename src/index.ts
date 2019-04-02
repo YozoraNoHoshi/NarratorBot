ecf8dded5d402e6db4db9c748bd2ea6ca698b942
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-04-02 11:51:50 -0700

    try/catch so bot doesnt crash

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,30 @@ import Bot from './Bot';
 
 const client: any = new discord.Client();
 
+client.on('ready', () => {
+    console.log('Drain your glass!');
+    client.user.setActivity('anime', { type: 'WATCHING' });
+});
+
 client.on(
     'message',
-    (message: any): string | void => {
+    async (message: any): Promise<string | void> => {
         if (message.author.bot) return;
-        if (message.content.trimLeft().startsWith(BOT_PREFIX)) {
-            let response: string | void = Bot.commandCenter(message);
-            if (response) message.channel.send(`\`${response}\``);
-        } else if (message.content.startsWith('[') && message.content.endsWith(']')) {
-            // emoji/image response
-            let emoji: string = message.content.slice(1, -1);
-            return;
+        try {
+            // standard commands that start with prefix (default '!')
+            if (message.content.trimLeft().startsWith(BOT_PREFIX)) {
+                let response: string | void = Bot.commandCenter(message);
+                if (response) message.channel.send(`\`${response}\``);
+            }
+            // emoji/image response, expects format '[emoji]'
+            else if (message.content.startsWith('[') && message.content.endsWith(']')) {
+                let emoji: string = message.content.slice(1, -1);
+                let response: string | void = await Bot.emoji(emoji);
+                if (response) message.channel.send(new discord.Attachment(response));
+            }
+        } catch (error) {
+            // if something blows up, bot doesn't crash
+            message.channel.send(`:blobderpy: \`Error ${error.status}: ${error.message}\` :blobderpy:`);
         }
     },
 );
