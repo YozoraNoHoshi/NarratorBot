4f2435d40819a45e21c524d74de6550bb4f0e94a
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-04-02 15:46:17 -0700

    added async methods for emojis

--- src/Bot.test.ts
+++ src/Bot.test.ts
@@ -2,19 +2,21 @@ import Bot from './Bot';
 import { FLIP_RESPONSES, EIGHT_BALL_RESPONSES } from './responses';
 import { BOT_PREFIX } from './config';
 
-test('Bot flip', () => {
-    expect(FLIP_RESPONSES).toContain(Bot.commandCenter({ content: `${BOT_PREFIX}flip` }));
+test('Bot flip', async () => {
+    expect(FLIP_RESPONSES).toContain(await Bot.commandCenter({ content: `${BOT_PREFIX}flip` }));
 });
-test('Bot 8ball', () => {
-    expect(EIGHT_BALL_RESPONSES).toContain(Bot.commandCenter({ content: `${BOT_PREFIX}8 what is life?` }));
-    expect(Bot.commandCenter({ content: `${BOT_PREFIX}8 what is life` })).toBe("That doesn't look like a question.");
+test('Bot 8ball', async () => {
+    expect(EIGHT_BALL_RESPONSES).toContain(await Bot.commandCenter({ content: `${BOT_PREFIX}8 what is life?` }));
+    expect(await Bot.commandCenter({ content: `${BOT_PREFIX}8 what is life` })).toBe(
+        "That doesn't look like a question.",
+    );
 });
-test('Bot die roll', () => {
-    expect(Bot.commandCenter({ content: `${BOT_PREFIX}roll blegh` })).toBe("You can't roll a blegh-sided die!");
-    expect(Bot.commandCenter({ content: `${BOT_PREFIX}roll` })).toContain('You rolled a');
-    expect(Bot.commandCenter({ content: `${BOT_PREFIX}roll -1` })).toBe('Maybe try something higher than -1?');
-    expect(Bot.commandCenter({ content: `${BOT_PREFIX}roll Infinity` })).toBe('No.');
+test('Bot die roll', async () => {
+    expect(await Bot.commandCenter({ content: `${BOT_PREFIX}roll blegh` })).toBe("You can't roll a blegh-sided die!");
+    expect(await Bot.commandCenter({ content: `${BOT_PREFIX}roll` })).toContain('You rolled a');
+    expect(await Bot.commandCenter({ content: `${BOT_PREFIX}roll -1` })).toBe('Maybe try something higher than -1?');
+    expect(await Bot.commandCenter({ content: `${BOT_PREFIX}roll Infinity` })).toBe('No.');
 });
-test('Bot help', () => {
-    expect(Bot.commandCenter({ content: `${BOT_PREFIX}help` })).toContain('You just called this command.');
+test('Bot help', async () => {
+    expect(await Bot.commandCenter({ content: `${BOT_PREFIX}help` })).toContain('You just called this command.');
 });
--- src/Bot.ts
+++ src/Bot.ts
@@ -12,10 +12,13 @@ class Bot {
         flip: Bot.flip,
         roll: Bot.dieRoll,
         '8': Bot.eightBall,
+        add: Bot.addEmoji,
+        delete: Bot.deleteEmoji,
+        emoji: Bot.getEmojiList,
     };
 
     // This method allows for dynamic calling of other methods based on the incoming message.
-    static commandCenter(message: any): string | void {
+    static async commandCenter(message: any): Promise<string | void> {
         let messageParts: string[] = message.content
             .trim()
             .slice(1)
@@ -25,7 +28,7 @@ class Bot {
             let action: any = Bot.methodMap[messageParts[0]];
             // calls method with first argument containing the rest of the message
             // >8 what is life? -> action('what is life?')
-            return action(messageParts.slice(1).join(' '));
+            return await action(messageParts.slice(1).join(' '));
         } else return;
     }
 
@@ -49,12 +52,49 @@ class Bot {
         return randomChoice(FLIP_RESPONSES);
     }
 
+    // rolls a virtual X sided die and returns the result. input must be positive and an actual number
     private static dieRoll(sides: string = '6'): string {
+        if (sides.toLowerCase().includes('infinity')) return 'No.';
         let input: number = Number(sides);
         if (isNaN(input)) return `You can't roll a ${sides}-sided die!`;
+        if (input < 0) return `Maybe try something higher than ${input}?`;
         let result = Math.ceil(Math.random() * input);
         return `You rolled a ${result}!`;
     }
+
+    private static async getEmojiList(): Promise<string> {
+        let result = await db.query(`SELECT name FROM emojis`);
+        return result.rows.reduce((acc, val) => {
+            return acc + `\n${val.name}`;
+        }, '');
+    }
+
+    private static async addEmoji(entry: string): Promise<string | void> {
+        let newEmoji: string[] = entry.split(' ');
+        let result = await db.query(`SELECT name, image FROM emojis WHERE name = $1`, [newEmoji[0]]);
+        if (result.rows[0]) {
+            // update emoji with new image
+            let response = await db.query(`UPDATE emojis SET image = $1 WHERE name = $2 RETURNING name`, [
+                newEmoji[1],
+                newEmoji[0],
+            ]);
+            return `Updated the ${response.rows[0].name} emoji.`;
+        } else {
+            // Add the emoji
+            let response = await db.query(`INSERT INTO emojis (name, image) VALUES ($1, $2)  RETURNING name`, [
+                newEmoji[0],
+                newEmoji[1],
+            ]);
+            return `Added the ${response.rows[0].name} emoji.`;
+        }
+    }
+
+    private static async deleteEmoji(name: string): Promise<string> {
+        let result = await db.query(`DELETE FROM emojis WHERE name = $1 RETURNING name`, [name]);
+        if (result.rows[0]) return `Deleted ${result.rows[0].name}`;
+        return `Could not find emoji named ${name}.`;
+    }
+
     // this command is accessed with a different syntax than normal commands.
     // Do not add this method to the methodMap
     static async emoji(name: string): Promise<string> {
