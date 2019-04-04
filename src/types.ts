69af15a12888e2121bea87c1b645a88200e26a5b
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-04-04 12:09:31 -0700

    added optional second argument to commands

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 // Either a function or another MethodMap which indicates a submenu
-export type BotCommand = (arg0: string) => any | MethodMap;
+export type BotCommand = (arg0: string, arg1?: any) => any | MethodMap;
 
 export type MethodMap = { [name: string]: BotCommand };
 export type helpShape = {
