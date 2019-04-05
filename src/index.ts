44915ea88cb192e1c8285e2b33a8676aa9d426ba
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-04-05 16:29:22 -0700

    bot no longer responds to DMs

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import discord = require('discord.js');
 import { TOKEN, BOT_PREFIX } from './config';
 import Bot from './models/Bot';
 import CustomEmoji from './models/Emoji';
+import { SendMsgEmbed } from './types';
 
 const client: any = new discord.Client();
 
@@ -13,22 +14,25 @@ client.on('ready', () => {
 client.on(
     'message',
     async (message: any): Promise<string | void> => {
-        if (message.author.bot) return;
         try {
-            // standard commands that start with prefix
-            if (message.content.trimLeft().startsWith(BOT_PREFIX)) {
-                message.noPrefix = message.content.slice(BOT_PREFIX.length);
-                let response: string | void = await Bot.commandCenter(message);
-                if (response) message.channel.send(response);
-            }
-            // emoji/image response, expects format '[emoji]'
-            else if (message.content.startsWith('[') && message.content.endsWith(']')) {
-                let emoji: string = message.content.slice(1, -1);
-                let response: string | void = await CustomEmoji.emoji(emoji);
-                if (response) message.channel.send(new discord.Attachment(response));
+            if (message.author.bot) return;
+            if (message.guild) {
+                // standard commands that start with prefix
+                if (message.content.trimLeft().startsWith(BOT_PREFIX)) {
+                    message.noPrefix = message.content.slice(BOT_PREFIX.length);
+                    let response: string | void | SendMsgEmbed = await Bot.commandCenter(message);
+                    if (response) message.channel.send(response);
+                }
+                // emoji/image response, expects format '[emoji]'
+                else if (message.content.startsWith('[') && message.content.endsWith(']')) {
+                    let emoji: string = message.content.slice(1, -1);
+                    let response: string | void = await CustomEmoji.emoji(emoji);
+                    if (response) message.channel.send(new discord.Attachment(response));
+                }
             }
         } catch (error) {
             // if something blows up, bot doesn't crash
+            error.status = error.status || 500;
             message.channel.send(`\`Error ${error.status}: ${error.message}\``);
         }
     },
