8249eb5e165a16cc5144d270e7d2a2d2036caf1f
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-04-06 22:26:10 -0700

    fixed log length display issue

--- src/models/MessageLog.ts
+++ src/models/MessageLog.ts
@@ -22,10 +22,11 @@ class MessageLog {
     static restoreMessages(message: DiscordMessage): SendMsgEmbed {
         // Removes up to 25 logged messages from the current channel and displays them in an embed
         // Note that this will not affect messages sent/deleted while the bot was offline.
-        let toBeRestored: number = Math.min(MessageLog._messageLog.length, 25);
-        let log: DiscordMessage[] = MessageLog._messageLog
-            .filter(loggedMessages => message.channel === loggedMessages.channel)
-            .splice(0, toBeRestored);
+        let log: DiscordMessage[] = MessageLog._messageLog.filter(
+            loggedMessages => message.channel === loggedMessages.channel,
+        );
+        let toBeRestored: number = Math.min(log.length, 25);
+        log = log.splice(0, toBeRestored);
         MessageLog.clearLog(log);
         let embed: DiscordEmbed = new RichEmbed()
             .setTitle('Message Log')
