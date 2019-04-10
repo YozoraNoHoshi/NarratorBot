2b04d9bbd2c3a25dda00dcc580cf4bde0f22d06b
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-04-09 17:23:47 -0700

    randomInt provides a range

--- src/helpers/randomInt/index.ts
+++ src/helpers/randomInt/index.ts
@@ -1,3 +1,6 @@
-export default function randomInt(num: number, up: boolean = false) {
-    return up ? Math.ceil(Math.random() * num) : Math.floor(Math.random() * num);
+export default function randomInt(min: number, max?: number): number {
+    //The maximum is exclusive and the minimum is inclusive
+    max = max ? Math.floor(max) : Math.ceil(min);
+    min = max ? Math.ceil(min) : 0;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
