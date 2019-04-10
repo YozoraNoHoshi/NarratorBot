50c2d0f1719e46dabab934c045ed5cf29cd7b9bd
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-04-09 17:33:31 -0700

    fixed random int fallback

--- src/helpers/randomInt/index.ts
+++ src/helpers/randomInt/index.ts
@@ -1,6 +1,11 @@
 export default function randomInt(min: number, max?: number): number {
     //The maximum is exclusive and the minimum is inclusive
-    max = max ? Math.floor(max) : Math.ceil(min);
-    min = max ? Math.ceil(min) : 0;
+    if (max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+    } else {
+        max = Math.ceil(min);
+        min = 0;
+    }
     return Math.floor(Math.random() * (max - min)) + min;
 }
