f8462df52db85a95b08bf20dab10e086eb93f0b8
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-04-09 20:19:10 -0700

    added test case

--- src/models/__tests__/MessageLog.test.ts
+++ src/models/__tests__/MessageLog.test.ts
@@ -1,7 +1,18 @@
 import MessageLog from '../MessageLog';
-import { Message } from 'discord.js';
 
 let date = new Date(2019, 0);
+
+test('it should have 0 fields for no deleted messages', async () => {
+    let response: any = MessageLog.restoreMessages(makeMessage(6));
+    expect(response).toHaveProperty('embed');
+    expect(response.embed).toHaveProperty('title', 'Message Log');
+    expect(response.embed).toHaveProperty('color');
+    expect(response.embed).toHaveProperty('timestamp');
+    expect(response.embed).toHaveProperty('description', '0 recently deleted messages');
+    expect(response.embed).toHaveProperty('fields');
+    expect(response.embed.fields.length).toBe(0);
+});
+
 test('It should restore messages that were deleted', async () => {
     MessageLog.addDeleted(makeMessage(1));
     MessageLog.addDeleted(makeMessage(2));
@@ -15,8 +26,7 @@ test('It should restore messages that were deleted', async () => {
     expect(response.embed).toHaveProperty('title', 'Message Log');
     expect(response.embed).toHaveProperty('color');
     expect(response.embed).toHaveProperty('timestamp');
-    expect(response.embed).toHaveProperty('description');
-    expect(response.embed.description).toContain('5 recently deleted messages');
+    expect(response.embed).toHaveProperty('description', '5 recently deleted messages');
     expect(response.embed).toHaveProperty('fields');
     expect(response.embed.fields.length).toBe(5);
     expect(response.embed.fields[0]).toHaveProperty('name', `another blah - ${date.toDateString()}`);
