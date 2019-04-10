ef3e43616335d2903e1e84d4a92603049331cdb8
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-04-09 21:12:36 -0700

    Anime starting point

---
+++ src/models/Anime.ts
@@ -0,0 +1,17 @@
+// import axios from 'axios'
+
+/* 
+Using API for anilist
+Should allow for basic info on anime, airing times, all anime in a season, etc etc. 
+Uses GraphQL.
+*/
+
+// https://anilist.gitbook.io/anilist-apiv2-docs/overview/overview
+
+class Anime {
+    static methodMap = {};
+}
+
+//
+
+export default Anime;
