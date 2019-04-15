fe39585488935a372ff4c75d583463ac7d379cd2
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-04-14 17:04:07 -0700

    get season anime working sans message formatting

--- src/models/Anime.ts
+++ src/models/Anime.ts
@@ -1,7 +1,8 @@
 import { MethodMap, DiscordMessage, SendMsgEmbed, ResponseMap } from '../types';
 import { RichEmbed } from 'discord.js';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { getByAiring, getBySeason } from '../queries/Anime';
+import createError from '../helpers/createError';
 
 /* 
 Using API for anilist
@@ -23,27 +24,29 @@ class Anime {
     private static BASE_URL: string = 'https://graphql.anilist.co';
 
     static async season(message: DiscordMessage): Promise<SendMsgEmbed> {
-        let { season, year } = Anime.getSeasonTag(message.noPrefix);
+        let { season, year } = Anime.getSeasonTags(message.noPrefix);
         let result = await Anime.requestToAniList(getBySeason, { season, year });
+        // format by page, reactions will be needed most likely
         let embed = new RichEmbed();
         return { embed };
     }
 
     private static async requestToAniList(query: string, variables: object): Promise<object> {
-        // Dont need to worry about error handling because bot has a global error handler yay.
-        let result = await axios.post(Anime.BASE_URL, { query, variables }, Anime.options);
-        console.log(result.data);
-        return result.data.data ? result.data.data : result.data.errors;
+        // Even though bot has global error handler, we want to reformat the error nicely.
+        try {
+            let result: AxiosResponse<any> = await axios.post(Anime.BASE_URL, { query, variables }, Anime.options);
+            return result.data.data ? result.data.data : result.data.errors;
+        } catch (error) {
+            console.log(error.response.data);
+            let errorMsg = error.response.data || 'An error occurred in making the request.';
+            throw createError(errorMsg, 400);
+        }
     }
 
-    private static getSeasonTag(seasons: string): { season: string; year: number } {
-        let messagePart = seasons.split(' ');
-        // First part is a year
-        if (!isNaN(Number(messagePart[0]))) {
-        }
-        let season = '';
-        let year = 0;
-        return { season, year };
+    private static getSeasonTags(seasons: string): { season: string; year: number } {
+        let date = new Date(seasons);
+        let season: string = ['WINTER', 'SPRING', 'SUMMER', 'FALL'][Math.floor(date.getMonth() / 4)];
+        return { season, year: date.getFullYear() };
     }
 
     private static options = {
