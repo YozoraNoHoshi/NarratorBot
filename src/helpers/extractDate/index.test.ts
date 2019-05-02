e383472831e5977c6ae983248fda1dbe147edafd
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-05-01 21:43:03 -0700

    fixed test

--- src/helpers/extractDate/index.test.ts
+++ src/helpers/extractDate/index.test.ts
@@ -2,6 +2,6 @@ import extractDate from '.';
 test('extractDate from a date object into hours/minutes/seconds', () => {
     let AMDate: Date = new Date(1556789100 * 1000);
     expect(extractDate(AMDate)).toBe(`02:25:00 AM`);
-    let PMDate: Date = new Date(1556728157911);
-    expect(extractDate(PMDate)).toBe(`09:29:17 PM`);
+    let PMDate: Date = new Date(1556772019128);
+    expect(extractDate(PMDate)).toBe(`09:40:19 PM`);
 });
