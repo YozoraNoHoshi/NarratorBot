3874c935327845c5ee6b33b1e7acb3286d210498
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-05-05 14:20:10 -0700

    mocks for axios (NYI)

---
+++ __fixtures__/aniList.ts
@@ -0,0 +1,51 @@
+export const tsukiGaKireiSearch: any = {
+    Media: {
+        title: {
+            romaji: 'Tsuki ga Kirei',
+            english: 'Tsukigakirei',
+        },
+        id: 98202,
+        coverImage: {
+            large: 'https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/98202-TUOEKKL0RI5T.png',
+        },
+        format: 'TV',
+        genres: ['Romance', 'Drama', 'Slice of Life'],
+        synonyms: ['as the moon so beautiful.'],
+        tags: [
+            {
+                name: 'School',
+            },
+            {
+                name: 'Coming of Age',
+            },
+            {
+                name: 'CGI',
+            },
+            {
+                name: 'Love Triangle',
+            },
+            {
+                name: 'School Club',
+            },
+        ],
+        startDate: {
+            year: 2017,
+            month: 4,
+            day: 7,
+        },
+        source: 'ORIGINAL',
+        season: 'SPRING',
+        episodes: 12,
+        duration: 25,
+        status: 'FINISHED',
+        description:
+            "The series focuses on characters Akane Mizuno and Kotarou Azumi, two third-year middle school students who become classmates for the first time. The series will depict each characters' growth and connection to the people around them, such as classmates, club-mates, teachers, and parents. The anime will also center on the youthful adolescent romance of the characters, who are hounded by change and uncertainty as the seasons inevitably pass.<br><br>\n\n(Source: ANN)",
+        studios: {
+            nodes: [
+                {
+                    name: 'feel.',
+                },
+            ],
+        },
+    },
+};
