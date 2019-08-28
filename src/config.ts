89dd7776a789f70e0dd4f29e7919ef2a3d5c662f
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-08-28 15:19:48 -0700

    envs take precedence for bot config

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@
 // require('dotenv').config();
 
 // The prefix used to trigger commands
-export const BOT_PREFIX: string = '$';
-export const EMOJI_PREFIX: string = '--';
-export const EMOJI_SUFFIX: string = '';
+export const BOT_PREFIX: string = process.env.BOT_PREFIX || '$';
+export const EMOJI_PREFIX: string = process.env.EMOJI_PREFIX || '--';
+export const EMOJI_SUFFIX: string = process.env.EMOJI_SUFFIX || '';
 // The token for the bot user - Requires a bot from discord's developer portal
 // DO NOT HARDCODE THE TOKEN HERE. STORE IT IN .ENV
 export const TOKEN: string | undefined = process.env.token;
