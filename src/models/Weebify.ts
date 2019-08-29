ba05aab1b7b18e4af938ae69a3e641ed077c0a5f
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-08-28 17:14:45 -0700

    proto

--- src/models/Weebify.ts
+++ src/models/Weebify.ts
@@ -1,25 +1,25 @@
-import axios from 'axios';
-import * as cheerio from 'cheerio';
-import { PrefixedMessage } from '../types';
+// import axios from 'axios';
+// import * as cheerio from 'cheerio';
+// import { PrefixedMessage } from '../types';
 
-class Weebify {
-  static getTranslateURL(query: string): string {
-    const textQuery = query.split(' ').join('%20');
-    return `https://translate.google.com/#view=home&op=translate&sl=en&tl=ja&text=${textQuery}`;
-  }
+// class Weebify {
+//   static getTranslateURL(query: string): string {
+//     const textQuery = query.split(' ').join('%20');
+//     return `https://translate.google.com/#view=home&op=translate&sl=en&tl=ja&text=${textQuery}`;
+//   }
 
-  static async text(string: string): Promise<string> {
-    const { data } = await axios.get(this.getTranslateURL(string));
+//   static async text(string: string): Promise<string> {
+//     const { data } = await axios.get(this.getTranslateURL(string));
 
-    const translate = cheerio.load(data);
+//     const translate = cheerio.load(data);
 
-    const textArr = translate('div.tlid-transliteration-content.transliteration-content.full');
-    return textArr.text();
-  }
+//     const textArr = translate('div.tlid-transliteration-content.transliteration-content.full');
+//     return textArr.text();
+//   }
 
-  static async transform(message: PrefixedMessage) {
-    return await Weebify.text(message.noPrefix);
-  }
-}
+//   static async transform(message: PrefixedMessage) {
+//     return await Weebify.text(message.noPrefix);
+//   }
+// }
 
-export default Weebify;
+// export default Weebify;
