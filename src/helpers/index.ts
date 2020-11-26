fe6b2e08a00fc68bebf86466f47e7285fabfda38
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2020-11-25 18:12:44 -0800

    Guild emojis helper fixed?

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,10 +1,10 @@
-import { Message } from "discord.js";
-import { PrefixedMessage } from "../types";
+import { Message } from 'discord.js';
+import { PrefixedMessage } from '../types';
 
-export function getUserDisplayName(id: string,message: PrefixedMessage | Message): string | undefined {
-  return message.guild?.member(id)?.displayName
+export function getUserDisplayName(id: string, message: PrefixedMessage | Message): string | undefined {
+  return message.guild?.member(id)?.displayName;
 }
 
 export function getEmojiString(name: string, message: PrefixedMessage | Message): string | undefined {
-  return message.guild?.emojis.resolve(name)?.toString()
+  return message.guild?.emojis.cache.find((e) => e.name === name)?.toString();
 }
