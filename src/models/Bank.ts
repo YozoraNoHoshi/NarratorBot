03a0862859e40288a0986bb4817db55d9a6d41db
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2020-11-25 18:25:33 -0800

    add emoji to credit balances

--- src/models/Bank.ts
+++ src/models/Bank.ts
@@ -1,12 +1,14 @@
 import admin from '../firebase';
 import { MethodMap, PrefixedMessage } from '../types';
-import { getUserDisplayName } from '../helpers';
+import { getEmojiString, getUserDisplayName } from '../helpers';
 
 export async function checkBalance(message: PrefixedMessage): Promise<string | null> {
   const doc = await admin.firestore().collection('users').doc(message.author.id).get();
   const credits = doc.get('credits') || 0;
   const name = getUserDisplayName(message.author.id, message);
-  return `${name} has ${credits} credits.`;
+
+  const creditEmoji = credits > 0 ? getEmojiString('blobderpy', message) : getEmojiString('blobstare', message);
+  return `${name} has ${credits} credits ${creditEmoji}`;
 }
 
 const BankMethods: MethodMap = {
