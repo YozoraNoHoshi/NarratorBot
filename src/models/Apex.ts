1f77f498919b974ebeb980ef92bf29a4d221237f
Nathan Chin <nchoshizora@gmail.com>
2022-07-25 15:28:07 -0700

    update map rotation command

--- src/models/Apex.ts
+++ src/models/Apex.ts
@@ -1,6 +1,34 @@
 import Axios from 'axios';
 import { APEX_MAP_ROTATION_URL } from '../constants';
-import { ResponseMap, MethodMap } from '../types';
+import { ResponseMap, MethodMap, SendMsgEmbed } from '../types';
+import { MessageEmbed } from 'discord.js';
+
+interface ApexMapResponse {
+  current: {
+    start: number;
+    end: number;
+    readableDate_start: string;
+    readableDate_end: string;
+    map: string;
+    code: string;
+    DurationInSecs: number;
+    DurationInMinutes: number;
+    asset: string;
+    remainingSecs: number;
+    remainingMins: number;
+    remainingTimer: string;
+  };
+  next: {
+    start: number;
+    end: number;
+    readableDate_start: string;
+    readableDate_end: string;
+    map: string;
+    code: string;
+    DurationInSecs: number;
+    DurationInMinutes: number;
+  };
+}
 
 class ApexLegendsCommands {
   static responseMap: ResponseMap = {
@@ -11,11 +39,16 @@ class ApexLegendsCommands {
     map: ApexLegendsCommands.mapRotation,
   };
 
-  static async mapRotation(): Promise<string> {
+  static async mapRotation(): Promise<SendMsgEmbed> {
     const url = `${APEX_MAP_ROTATION_URL}?auth=${process.env.APEX_API_KEY}`;
-    const { data } = await Axios.get(url);
+    const { data }: { data: ApexMapResponse } = await Axios.get(url);
+    const { current, next } = data;
 
-    return JSON.stringify(data);
+    const embed = new MessageEmbed().setTitle('Apex Legends Map Rotation');
+    embed.addField('Current Map', `${current.map} (${current.remainingTimer})`);
+    embed.addField('Next Map', `${next.map} (${next.DurationInMinutes})`);
+    embed.setImage(current.asset);
+    return { embed };
   }
 }
 
